f8fd44153d5c429e0104d974730a69f70c058d38
FilipeamTeixeira <Filipe.MarquesTeixeira@Ugent.be>
2021-05-02 21:49:40 +0200

    first commit

---
+++ network_analysis.R
@@ -0,0 +1,15 @@
+# Network Analysis Seminar - FLAMES
+# 3rd May 2021
+
+#### Load packages and data ####
+library(data.table)
+library(tidyverse)
+library(igraph)
+library(igraphdata)
+
+data(package="igraphdata") #check available datasets
+
+data(Koenigsberg)
+data(USairports)
+Koenigsberg
+
