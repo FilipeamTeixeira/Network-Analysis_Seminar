b3aaefb411fb6fc8b0cf7010651d89129849893d
FilipeamTeixeira <Filipe.MarquesTeixeira@Ugent.be>
2021-05-02 21:54:32 +0200

    added install packages

--- network_analysis.R
+++ network_analysis.R
@@ -1,6 +1,11 @@
 # Network Analysis Seminar - FLAMES
 # 3rd May 2021
 
+#### Install Packages ####
+
+install.packages(c("data.table", "tidyverse", "igraph",
+                   "igraphdata", "visNetwork", "ggraph"))
+
 #### Load packages and data ####
 library(data.table)
 library(tidyverse)
